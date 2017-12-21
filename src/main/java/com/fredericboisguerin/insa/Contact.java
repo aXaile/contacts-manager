eb9231351f4c4acb37719ee06e5ab80087e99aef
aXaile <aaloisi@etud.insa-toulouse.fr>
2017-12-21 16:08:35 +0100

    addContact

---
+++ src/main/java/com/fredericboisguerin/insa/Contact.java
@@ -0,0 +1,13 @@
+package com.fredericboisguerin.insa;
+
+public class Contact {
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Contact(String name,String email,String phoneNumber){
+        this.name=name;
+        this.phoneNumber=phoneNumber;
+        this.email=email;
+    }
+}
--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -1,8 +1,15 @@
 package com.fredericboisguerin.insa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactsManager {
 
+    private List<Contact> contactList = new ArrayList<>();
+
     public void addContact(String name, String email, String phoneNumber) {
+        Contact contact=new Contact(name,email,phoneNumber);
+        contactList.add(contact);
 
     }
 
